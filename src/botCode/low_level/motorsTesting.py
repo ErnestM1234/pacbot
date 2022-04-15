827c0e827cf34ffd4230d14600f59421bce2b5ce
Ernest McCarter <ernestm@princeton.edu>
2022-04-15 18:04:25 -0400

    simple custom movement code for our bot, needs a lot of fine tuning

---
+++ src/botCode/low_level/motorsTesting.py
@@ -0,0 +1,51 @@
+from .sensorTesting import *
+
+WALL_STOP_DIST = 5 # cm
+
+class AvailableMotors(Enum):
+    LEFT = 0
+    RIGHT = 1
+
+
+# this does not correspond directly to real motor, this is more of an abstract motor
+class ArduinoMotor:
+    def __init__(self, arduino, motor_state):
+        self.motor_state = motor_state
+        self.arduino = arduino
+
+    def move(self, dir, pwr):
+        if self.motor_state == AvailableMotors.RIGHT:
+            self.arduino.write(dir, pwr, MotorDirection.MAINTAIN, 0.0)
+        elif self.motor_state == AvailableMotors.LEFT:
+            self.arduino.write(MotorDirection.MAINTAIN, 0.00, dir, pwr)
+
+
+class ArduinoMotors:
+    def __init__(self):
+        self.arduino = ArduinoComms()
+        self.heading = 0.0
+        self.right_motor = ArduinoMotor(self.arduino, AvailableMotors.RIGHT)
+        self.left_motor = ArduinoMotor(self.arduino, AvailableMotors.LEFT)
+
+    def turn_right(self):
+        self.right_motor.move(MotorDirection.FORWARD, 1) # idk if 1 works but thats what it be for now
+        self.left_motor.move(MotorDirection.BACKWARD, 1) # idk if 1 works but thats what it be for now
+
+    def turn_left(self):
+        self.right_motor.move(MotorDirection.BACKWARD, 1) # idk if 1 works but thats what it be for now
+        self.left_motor.move(MotorDirection.FORWARD, 1) # idk if 1 works but thats what it be for now
+
+    def moveForwards(self, pwr):
+        self.arduino.write(MotorDirection.FORWARD, pwr, MotorDirection.FORWARD, pwr)
+
+    # goes forwards and stops at a wall
+    def stopAtWall(self):
+        self.goForwards(1)
+        while True:
+            self.arduino.read()
+            if self.arduino.readSensor("FORWARD_DIST") < WALL_STOP_DIST:
+                self.stop()
+                break
+
+    def stop(self):
+        self.arduino.write(MotorDirection.STOP, 0.0, MotorDirection.STOP, 0.0)
--- src/botCode/low_level/sensorTesting.py
+++ src/botCode/low_level/sensorTesting.py
@@ -1,11 +1,122 @@
+from enum import Enum
 import serial
+import json
 
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
-    ser.reset_input_buffer()
+"""
+SENSOR INPUTS:
+
+LEFT_ENCODER    - float (cm)
+RIGHT_ENCODER   - float (cm)
+
+LEFT_DISTANCE   - float (cm)
+RIGHT_DISTANCE  - float (cm)
+
+LEFT_DIAG_DIST  - float (cm)
+RIGHT_DIAG_DIST - float (cm)
+
+FORWARD_DIST    - float (cm)
+
+HEADING         - float (degree)
+
+input json string format: '{ 
+    "LEFT_ENCODER":0.0, "RIGHT_ENCODER":0.0, 
+    "LEFT_DISTANCE":0.0, "RIGHT_DISTANCE":0.0,
+    "LEFT_DIAG_DIST":0.0, "RIGHT_DIAG_DIST":0.0,
+    "FORWARD_DIST":0.0, "HEADING":0.0
+    }'
+
+
+MOTOR OUTPUTS:
+
+RIGHT_MOTOR_DIRECTION       - MotorDirection
+RIGHT_MOTOR_POWER           - float (wats?? volts?? idk)
+
+LEFT_MOTOR_DIRECTION       - MotorDirection
+LEFT_MOTOR_POWER           - float (wats?? volts?? idk)
+
+output json string format: '{
+    "RIGHT_MOTOR_DIRECTION":0.0,
+    "RIGHT_MOTOR_POWER":0.0,
+    "LEFT_MOTOR_DIRECTION":0.0,
+    "LEFT_MOTOR_POWER":0.0,
+}'
+
+
+"""
+
+SENSOR_NAMES = {
+    "LEFT_ENCODER",     "RIGHT_ENCODER", 
+    "LEFT_DISTANCE",    "RIGHT_DISTANCE",
+    "LEFT_DIAG_DIST",   "RIGHT_DIAG_DIST",
+    "FORWARD_DIST",     "HEADING"
+}
+
+
+class MotorDirection(Enum):
+    BACKWARD = 0
+    FORWARD = 1
+    STOP = 2
+    MAINTAIN = 3 # maintain current power
+
+class ArduinoComms:
+
+
+    def __init__(self):
+        self.sensors = {
+            "LEFT_ENCODER":0.0,     "RIGHT_ENCODER":0.0, 
+            "LEFT_DISTANCE":0.0,    "RIGHT_DISTANCE":0.0,
+            "LEFT_DIAG_DIST":0.0,   "RIGHT_DIAG_DIST":0.0,
+            "FORWARD_DIST":0.0,     "HEADING":0.0
+        }
+        self.ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+        self.ser.reset_input_buffer()
+
+    def readSensor(self, sensor):
+        if sensor not in SENSOR_NAMES:
+            return None
+        return self.sensors[sensor]
+
+    # update sensors
+    def read(self):
+        if self.ser.in_waiting > 0:
+            sensor_input = self.ser.readline().decode('utf-8').rstrip()
+            print(sensor_input)
+            try:
+                sensor_input_json = json.loads(sensor_input)
+                for sensor_name, value in sensor_input_json.items():
+                    self.sensors[sensor_name] = value
+            except:
+                print("input not in valid JSON format or maybe something else went wrong idk")
+
+        return self.sensors
+
+    def write(self, r_dir, r_pwr, l_dir, l_pwr):
+        # todo: check that values are valid
+        output = {
+            "RIGHT_MOTOR_DIRECTION":r_dir,
+            "RIGHT_MOTOR_POWER":r_pwr,
+            "LEFT_MOTOR_DIRECTION":l_dir,
+            "LEFT_MOTOR_POWER":l_pwr,
+        }
+        self.ser.write(json.dumps(output).encode('utf-8'))
+
+    def print_all_values(self):
+        print(self.sensors)
+
+
+# if __name__ == '__main__':
+#     ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
+#     ser.reset_input_buffer()
     
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            ser.write('Hello from PI\n'.encode('utf-8'))
-            print(line)
+#     while True:
+#         if ser.in_waiting > 0:
+#             sensor_input = ser.readline().decode('utf-8').rstrip()
+#             print(sensor_input)
+            
+#             try:
+#                 sensor_input_json = json.dumps(sensor_input)
+#             except:
+#                 print("input not in valid JSON format")
+            
+#             # ser.write('Hello from PI\n'.encode('utf-8'))
+
