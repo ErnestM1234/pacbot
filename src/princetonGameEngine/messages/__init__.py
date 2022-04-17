87ff5d7e6acf410fd95341a754f8e08c72112bfb
Ernest McCarter <ernestm@princeton.edu>
2022-04-17 16:33:04 -0400

    added messaging system for new gameeng server

--- src/princetonGameEngine/messages/__init__.py
+++ src/princetonGameEngine/messages/__init__.py
@@ -1,17 +1,21 @@
 from enum import Enum
 from .pacmanState_pb2 import PacmanState
 from .lightState_pb2 import LightState
+from .pacmanCommand_pb2 import PacmanCommand
 
 class MsgType(Enum):
     LIGHT_STATE = 0
     PACMAN_LOCATION = 1
     FULL_STATE = 2
+    PACMAN_COMMAND = 3
 
 message_buffers = {
+    MsgType.PACMAN_COMMAND: PacmanCommand,
     MsgType.FULL_STATE: PacmanState,
     MsgType.PACMAN_LOCATION: PacmanState.AgentState,
     MsgType.LIGHT_STATE: LightState
 }
 
 
-__all__ = ['MsgType', 'message_buffers', 'PacmanState', 'LightState']
+__all__ = ['MsgType', 'message_buffers', 'PacmanState', 'LightState',
+            'PacmanCommand']
