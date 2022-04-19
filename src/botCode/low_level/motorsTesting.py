7506725500629145c9a7a7763913cbd1ead1e23f
Ernest McCarter <ernestm@princeton.edu>
2022-04-19 19:47:05 -0400

    MAX POWERRRRRRRRR

--- src/botCode/low_level/motorsTesting.py
+++ src/botCode/low_level/motorsTesting.py
@@ -43,7 +43,7 @@ class ArduinoMotors:
     Rotate robot right
     """
     def rotate_right(self):
-        self.arduino.write(MotorDirection.FORWARDS, 200, MotorDirection.BACKWARDS, 200)
+        self.arduino.write(MotorDirection.FORWARDS, 255, MotorDirection.BACKWARDS, 255)
 
     """ rotate_left()
     input:  void
@@ -51,7 +51,7 @@ class ArduinoMotors:
     Rotate robot left
     """
     def rotate_left(self):
-        self.arduino.write(MotorDirection.BACKWARDS, 200, MotorDirection.FORWARDS, 200)
+        self.arduino.write(MotorDirection.BACKWARDS, 255, MotorDirection.FORWARDS, 255)
 
     # ------------------------ Turning ------------------------ #
     """ turn_right()
@@ -162,7 +162,7 @@ class ArduinoMotors:
     moves robot forward at power 20 (note this does not check for obstacles)
     """
     def move_forwards(self):
-        self.arduino.write(MotorDirection.FORWARDS, 200, MotorDirection.FORWARDS, 200)
+        self.arduino.write(MotorDirection.FORWARDS, 255, MotorDirection.FORWARDS, 255)
 
     """ move_backwards()
     input:  void
@@ -170,7 +170,7 @@ class ArduinoMotors:
     moves robot backwards at power 20 (note this does not check for obstacles)
     """
     def move_backwards(self):
-        self.arduino.write(MotorDirection.BACKWARDS, 200, MotorDirection.BACKWARDS, 200)
+        self.arduino.write(MotorDirection.BACKWARDS, 255, MotorDirection.BACKWARDS, 255)
 
     """ move_dist()
     input:  dist - mm to move forwards
