0e8ca7cc8cab91014d15df569a97c0e1b96cc296
Ernest McCarter <ernestm@princeton.edu>
2022-04-21 01:00:11 -0400

    heading error

--- src/botCode/low_level/controlBotTest.py
+++ src/botCode/low_level/controlBotTest.py
@@ -18,7 +18,7 @@ def mop(bot):
     
         #char = input()
         #if char == "n":
-        print("heading: " + bot.arduino.getHeading())
+        print("heading: " + str(bot.arduino.getHeading()))
         # print("MAG X: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_X")) + " MAG Y: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Y")) + " MAG Z: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Z")))
         # if char == "f":
         #     bot.move_forwards()
