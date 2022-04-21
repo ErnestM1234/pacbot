0d9232bf9cce505f7b9d02d46eba162b4c3be7bf
Ernest McCarter <ernestm@princeton.edu>
2022-04-20 23:39:26 -0400

    zoooom

--- src/botCode/low_level/sensorTesting.py
+++ src/botCode/low_level/sensorTesting.py
@@ -455,17 +455,17 @@ class ArduinoComms:
     """
     def read(self):
         # read from input buffer
-        if self.ser.in_waiting > 0:
-            sensor_input = self.ser.readline().decode('ascii').rstrip()
-            # this is to ensure that we are receiving a json formatted string
-            for key, value in json.loads(sensor_input).items():
-                self.sensors[key] = value
+        # if self.ser.in_waiting > 0:
+        sensor_input = self.ser.readline().decode('ascii').rstrip()
+        # this is to ensure that we are receiving a json formatted string
+        for key, value in json.loads(sensor_input).items():
+            self.sensors[key] = value
             # print("ACC_X: " + str(self.sensors["ACC_X"]) + " ACC_Y: " + str(self.sensors["ACC_Y"]) + " ACC_Z: " + str(self.sensors["ACC_Z"]))
             # print("GYRO_X: " + str(self.sensors["GYRO_X"]).zfill(8) + " GYRO_Y: " + str(self.sensors["GYRO_Y"]).zfill(8) + " GYRO_Z: " + str(self.sensors["GYRO_Z"]).zfill(8))
             # print("MAG_X: " + str(self.sensors["MAG_X"]) + " MAG_Y: " + str(self.sensors["MAG_Y"]) + " MAG_Z: " + str(self.sensors["MAG_Z"]))
 
         # update odometer
-        self.updateOdometer()
+        # self.updateOdometer()
         # print("odometer:" + str(self.getOdometer()))
 
         return self.sensors
