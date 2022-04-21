46215a0074c29c08f06c6e16d9427a7f6a1b8811
Ernest McCarter <ernestm@princeton.edu>
2022-04-20 23:51:17 -0400

    mag measure

--- src/botCode/low_level/controlBotTest.py
+++ src/botCode/low_level/controlBotTest.py
@@ -9,15 +9,16 @@ def mop(bot):
     bot.stop()
     while True:
         bot.arduino.read()
-        heading = bot.arduino.getHeading()
-        print("GYRO_X: " + "{:4.2f}".format(heading[0]) + " GYRO_Y: " + "{:4.2f}".format(heading[1]) + " GYRO_Z: " + "{:4.2f}".format(heading[2]))
+        # heading = bot.arduino.getHeading()
+        # print("GYRO_X: " + "{:4.2f}".format(heading[0]) + " GYRO_Y: " + "{:4.2f}".format(heading[1]) + " GYRO_Z: " + "{:4.2f}".format(heading[2]))
         # print(str(bot.arduino.getOdometer()))
         if bot.arduino.readSensor("FORWARD_DIST") < 50:
             bot.arduino.resetOdometer()
             print("reset odometer")
     
-        
-        # char = input()
+        char = input()
+        if char == "n":
+            print("MAG X: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_X")) + " MAG Y: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Y")) + " MAG Z: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Z")))
         # if char == "f":
         #     bot.move_forwards()
         #     print("forwards")
