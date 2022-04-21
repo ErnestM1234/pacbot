a89d0bdf4a70e04bdde8d616b72b9a55407acc13
Ernest McCarter <ernestm@princeton.edu>
2022-04-21 00:59:14 -0400

    get heading()

--- src/botCode/low_level/controlBotTest.py
+++ src/botCode/low_level/controlBotTest.py
@@ -18,7 +18,8 @@ def mop(bot):
     
         #char = input()
         #if char == "n":
-        print("MAG X: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_X")) + " MAG Y: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Y")) + " MAG Z: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Z")))
+        print("heading: " + bot.arduino.getHeading())
+        # print("MAG X: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_X")) + " MAG Y: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Y")) + " MAG Z: " + "{:6.2f}".format(bot.arduino.readSensor("MAG_Z")))
         # if char == "f":
         #     bot.move_forwards()
         #     print("forwards")
