1f403fe5f36fe0f63adbaad686a4c17a125d18ef
Ernest McCarter <ernestm@princeton.edu>
2022-04-23 04:20:54 -0400

    testing

--- src/botCode/messages/__init__.py
+++ src/botCode/messages/__init__.py
@@ -2,20 +2,27 @@ from enum import Enum
 from .lightState_pb2 import LightState
 from .pacmanState_pb2 import PacmanState
 from .pacmanCommand_pb2 import PacmanCommand
+from .test_pb2 import Test
+from .pacCommand_pb2 import PacCommand
 
 
 class MsgType(Enum):
     LIGHT_STATE = 0
     PACMAN_LOCATION = 1
     PACMAN_COMMAND = 2
+    TEST = 3
+    PAC_COMMAND = 4
+
 
 
 message_buffers = {
     MsgType.LIGHT_STATE: LightState,
     MsgType.PACMAN_LOCATION: PacmanState.AgentState,
-    MsgType.PACMAN_COMMAND: PacmanCommand
+    MsgType.PACMAN_COMMAND: PacmanCommand,
+    MsgType.TEST: Test,
+    MsgType.PAC_COMMAND: PacCommand
 }
 
 
 __all__ = ['MsgType', 'message_buffers', 'LightState', 'PacmanState',
-           'PacmanCommand']
+           'PacmanCommand', 'Test', 'PacCommand']
