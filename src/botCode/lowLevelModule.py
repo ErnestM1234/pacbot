5e1dd12c3d476f21df4f950083e02027f8c92bc6
Ernest McCarter <ernestm@princeton.edu>
2022-04-23 14:10:03 -0400

    mess

--- src/botCode/lowLevelModule.py
+++ src/botCode/lowLevelModule.py
@@ -67,6 +67,7 @@ class LowLevelCommandModule(rm.ProtoModule):
         return True
             
     def msg_received(self, msg, msg_type):
+        print("mess received")
         if msg_type == MsgType.PAC_COMMAND:
             self.command_queue.append((msg.command.direction, msg.command.forwards_distance))
             # print(str(self.command_queue))
