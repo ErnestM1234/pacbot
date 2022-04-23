a46a6ad1d7a1fefb7a89cb77ca9ebbecbff5c584
ianh3727 <71193028+ianh3727@users.noreply.github.com>
2022-04-23 11:18:52 -0400

    Reworked low level module

--- src/botCode/lowLevelModule.py
+++ src/botCode/lowLevelModule.py
@@ -1,124 +1,81 @@
 #!/usr/bin/env python3
 
 import os
+import time
 import robomodules as rm
 import variables as var
 from grid import grid
-from low_level.motors import Motors
+from deque import *
 from messages import MsgType, message_buffers, LightState, PacmanCommand
+from sensorTesting import *
+from messages.ack_pb2 import Ack
+from messages.pacCommand_pb2 import PacCommand
 
 ADDRESS = os.environ.get("LOCAL_ADDRESS","localhost")
 PORT = os.environ.get("LOCAL_PORT", 11295)
 
 FREQUENCY = 60
 
-class LowLevelModule(rm.ProtoModule):
+class LowLevelCommandModule(rm.ProtoModule):
     def __init__(self, addr, port):
-        self.subscriptions = [MsgType.PACMAN_COMMAND]
+      	self.arduino = ArduinoComms()
+        self.subscriptions = [MsgType.PAC_COMMAND]
         super().__init__(addr, port, message_buffers, MsgType, FREQUENCY, self.subscriptions)
-        self.current_command = None
-        self.current_location = (13, 7)
-        self.motors = Motors()
-        self.current_dir = PacmanCommand.EAST
+        self.command_queue = deque()
+        self.current_command = ("forwards", 0) # in the form of a tuple
+        self.pending_completion = False
         
-    def _should_turn_left(self, cmd):
-        return \
-            (self.current_dir == PacmanCommand.NORTH and cmd  == PacmanCommand.WEST) or \
-            (self.current_dir == PacmanCommand.SOUTH and cmd  == PacmanCommand.EAST) or \
-            (self.current_dir == PacmanCommand.EAST and cmd  == PacmanCommand.NORTH) or \
-            (self.current_dir == PacmanCommand.WEST and cmd  == PacmanCommand.SOUTH)
-
-    def _should_turn_right(self, cmd):
-        return \
-            (self.current_dir == PacmanCommand.NORTH and cmd  == PacmanCommand.EAST) or \
-            (self.current_dir == PacmanCommand.SOUTH and cmd  == PacmanCommand.WEST) or \
-            (self.current_dir == PacmanCommand.EAST and cmd  == PacmanCommand.SOUTH) or \
-            (self.current_dir == PacmanCommand.WEST and cmd  == PacmanCommand.NORTH)
-
-    def _should_reverse(self, cmd):
-        return \
-            (self.current_dir == PacmanCommand.NORTH and cmd  == PacmanCommand.SOUTH) or \
-            (self.current_dir == PacmanCommand.SOUTH and cmd  == PacmanCommand.NORTH) or \
-            (self.current_dir == PacmanCommand.EAST and cmd  == PacmanCommand.WEST) or \
-            (self.current_dir == PacmanCommand.WEST and cmd  == PacmanCommand.EAST)
-
     def _move_forward(self):
-        self.motors.move_cells(1)
+        self.arduino.write(FORWARDS, 1, False, False)
 
-    def _turn_right(self):
-        self.motors.turn_right() # change physical direction
-        if self.current_dir == PacmanCommand.EAST: # update inner state of robot accordingly
-            self.current_dir = PacmanCommand.SOUTH
-        elif self.current_dir == PacmanCommand.SOUTH:
-            self.current_dir = PacmanCommand.WEST
-        elif self.current_dir == PacmanCommand.WEST:
-            self.current_dir = PacmanCommand.NORTH
-        else:
-            self.current_dir = PacmanCommand.EAST
-            
     def _turn_left(self):
-        self.motors.turn_left()
-        if self.current_dir == PacmanCommand.EAST:
-            self.current_dir = PacmanCommand.NORTH
-        elif self.current_dir == PacmanCommand.NORTH:
-            self.current_dir = PacmanCommand.WEST
-        elif self.current_dir == PacmanCommand.WEST:
-            self.current_dir = PacmanCommand.SOUTH
-        else:
-            self.current_dir = PacmanCommand.EAST
-
-    def _turn_around(self):
-        #TODO: Turn 180 degrees
-        if self.current_dir == PacmanCommand.EAST:
-            self.current_dir = PacmanCommand.WEST
-        elif self.current_dir == PacmanCommand.NORTH:
-            self.current_dir = PacmanCommand.SOUTH
-        elif self.current_dir == PacmanCommand.WEST:
-            self.current_dir = PacmanCommand.EAST
-        else:
-            self.current_dir = PacmanCommand.NORTH
-    
-    def _reverse(self):
-        self.motors.reverse(1)
+        self.arduino.write(ROTATE, 0, True, False)
+            
+    def _turn_right(self):
+        self.arduino.write(ROTATE, 0, False, True)
 
     def _execute_command(self):
-        if self.current_command:
-            cmd = self.current_command
-            self.current_command = None
-            if cmd == PacmanCommand.STOP:
-                self.motors.stop()
-                return
-            if self._should_turn_left(cmd):
-                self._turn_left()
-            elif self._should_turn_right(cmd):
-                self._turn_right()
-            if self._should_reverse(cmd):
-                self._reverse()
-            else:
-                self._move_forward()
-            if cmd == PacmanCommand.EAST:
-                self.current_location = (self.current_location[0] + 1, self.current_location[1])
-            elif cmd == PacmanCommand.WEST:
-                self.current_location = (self.current_location[0] - 1, self.current_location[1])
-            elif cmd == PacmanCommand.NORTH:
-                self.current_location = (self.current_location[0], self.current_location[1] + 1)
-            elif cmd == PacmanCommand.SOUTH:
-                self.current_location = (self.current_location[0], self.current_location[1] - 1)
-
+        
+        if !self.current_command:
+        	return False
+          
+        self.pending_completion = True        
+        cmd = self.current_command
+        self.current_command = None
+        if (len(self.command_queue) > 0):
+            self.current_command = self.command_queue.popleft()
+        
+        while not self.arduino.checkAck():
+          	if (cmd[0] == PacCommand.FORWARDS):
+            	self.arduino.write(FORWARDS, cmd[1], False, False)
+          	else if (cmd[0] == PacCommand.LEFT):
+          		self.arduino.write(ROTATE, 0, True, False)
+          	else if (cmd[0] == PacCommand.RIGHT):
+              	self.arduino.write(ROTATE, 0, False, True)
+                
+        self.pending_completion = False
+        
+        ack = Ack()
+        ack.hasAck = 1
+        self.write(ack.SerializeToString(), MsgType.ACK())
+        return True
+            
     def msg_received(self, msg, msg_type):
-        if msg_type == MsgType.PACMAN_COMMAND:
-            self.current_command = msg.dir
-
+        if msg_type == MsgType.PAC_COMMAND:
+            self.command_queue.append((msg.command.direction, msg.command.forwards_distance))
+        return True
+    
     def tick(self):
-        if self.current_command:
+        if self.current_command and not self.pending_completion:
             self._execute_command()
-
+            
     def kill(self):
-        self.motors.stop()
-
+      	output = "{sto}"
+    	while True:
+          self.arduino.write(output.encode('utf-8'))
 
 def main():
-    module = LowLevelModule(ADDRESS, PORT)
+    module = LowLevelCommandModule(ADDRESS, PORT)
     try:
         module.run()
     except KeyboardInterrupt:
